7b9fad81889fbfcd07ef27c58be88b2bfa4a98c9
Maxy15 <max.yanez@uc.cl>
2023-02-10 16:36:24 -0300

    add deleteLetters

--- Frontend-buttons/components/NumberInput.js
+++ Frontend-buttons/components/NumberInput.js
@@ -80,17 +80,23 @@ export default function NumberInput({ value, max, min, softMin, softMax, step })
         else {
             setIsRed(false);
         }
-        
     }
 
     const timer = () => {
         setTimeout(checkInput, 800);
     }
 
+    const deleteLetters = () => {
+        // Limpiar letras
+        const input = document.getElementById('number');
+        input.value = input.value.replace(/[^\d,]/g,'');
+        timer();
+    }
+
     return(
         <div className="flex flex-row justify-center items-center">
             <button id='button-minus' onClick={() => minus(min, softMin)} disabled={isMinusDisabled} className={isMinusDisabled ? "font-eaves-ultra w-12 h-12 pt-px bg-neutral-900 text-white text-5xl rounded-lg mr-4 cursor-pointer" : "font-eaves-ultra w-12 h-12 pt-px bg-red-600 text-white text-5xl rounded-lg mr-4 active:bg-red-900 hover:bg-red-300 cursor-pointer"}>-</button>
-            <input type='text' id='number' onChange={timer} defaultValue={value} className={isRed ? "font-eaves-regular text-center w-3/12 h-12 bg-red-300 text-red-900 text-2xl border-2 border-b-red-500 focus:border-red-500" : "font-eaves-regular text-center text-neutral-900 w-3/12 h-12 bg-neutral-300 text-2xl border-2 border-b-neutral-900 focus:border-neutral-900"}></input>
+            <input type="text" id='number' onChange={deleteLetters} defaultValue={value} className={isRed ? "font-eaves-regular text-center w-3/12 h-12 bg-red-300 text-red-900 text-2xl border-2 border-b-red-500 focus:border-red-500" : "font-eaves-regular text-center text-neutral-900 w-3/12 h-12 bg-neutral-300 text-2xl border-2 border-b-neutral-900 focus:border-neutral-900"}></input>
             <button id='button-plus' onClick={() => plus(max, softMax)} disabled={isPlusDisabled} className={isPlusDisabled ? "font-eaves-ultra w-12 h-12 pt-px bg-neutral-900 text-white text-5xl rounded-lg ml-4 cursor-pointer" : "font-eaves-ultra w-12 h-12 pt-px bg-red-600 text-white text-5xl rounded-lg ml-4 active:bg-red-900 hover:bg-red-300 cursor-pointer"}>+</button>
         </div>
     )
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Test técnico para Galilea :house:
 Este repositorio tiene tres carpetas, las cuales contienen lo siguiente:
 * Frontend-buttons: Botones desarrollados en NextJS, donde se encuentra el componente *NumberInput* que recibe los parámetros ```{ value, max, min, softMin, softMax, step }```. Dentro de la carpeta se encuentran las instrucciones para ver la página web.
-* Backend: Función *Compute25* desarrollada en Python
+* Lógica: Función *Compute25* desarrollada en Python
 * SQL: Consulta del problema creada en Postgres
